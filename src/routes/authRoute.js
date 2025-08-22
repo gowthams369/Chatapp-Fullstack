27f86d63f42d4806ef99b83ab1cd8b678fd64602
gowthams369 <project3690000@gmail.com>
2025-08-22 18:54:56 +0530

    added update and check user endpoints

--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -1,8 +1,18 @@
 import express from "express";
-import { login, logout, signUp } from "../controller/authController.js";
+import {
+  checkAuth,
+  login,
+  logout,
+  signUp,
+  updateProfile,
+} from "../controller/authController.js";
+import { userAuth } from "../middleware/authMiddleware.js";
 
 export const authRouter = express.Router();
 
 authRouter.post("/user/signup", signUp);
 authRouter.post("/user/login", login);
 authRouter.post("/user/logout", logout);
+authRouter.put("/user/update-profile", userAuth, updateProfile);
+
+authRouter.get("/user/checkauth", userAuth, checkAuth);
