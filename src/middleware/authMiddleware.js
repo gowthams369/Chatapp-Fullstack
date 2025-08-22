aa4e1db9bc86631f7669077d02cc13019a11efa6
gowthams369 <project3690000@gmail.com>
2025-08-22 18:54:37 +0530

    solved some issues in auth

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -1,17 +1,18 @@
 import { PrismaClient } from "@prisma/client";
+import jwt from "jsonwebtoken";
 
 const prisma = new PrismaClient();
 
 export const userAuth = async (req, res, next) => {
   try {
-    const token = req.cookies;
+    const token = req.cookies.token;
     if (!token) {
       res.status(403).json("token missing");
     }
-    const decodedMesage = await jwt.verify(token, process.env.JWT_SECRET);
-    const { userId } = decodedMesage;
+    const decodedMesage = jwt.verify(token, process.env.JWT_SECRET);
+    const { id } = decodedMesage;
     const user = await prisma.user.findUnique({
-      where: { id: userId },
+      where: { id: id },
     });
     if (!user) {
       throw new Error("user not found");
