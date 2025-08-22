ad414b803e1daab79928366afd8ecb8814e1fd86
gowthams369 <project3690000@gmail.com>
2025-08-22 09:56:16 +0530

    added login with jwt tocken genration

--- src/controller/authController.js
+++ src/controller/authController.js
@@ -1,4 +1,5 @@
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
@@ -27,5 +28,41 @@ export const signUp = async (req, res) => {
   } catch (error) {
     console.log("Error:", error);
     res.status(500).json({ message: "Error in signup", error: error.message });
+  }
+};
+
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await prisma.user.findUnique({
+      where: {
+        email: email,
+      },
+    });
+    if (!user) {
+      res.status(401).json("Invalid credentials");
+    }
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      res.status(400).json("invalid credentials");
+    }
+    const token = jwt.sign(
+      {
+        id: user.id,
+        email: user.email,
+      },
+      process.env.JWT_SECRET,
+      { expiresIn: "7d" }
+    );
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "strict",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+    res.send(user);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
   }
 };
