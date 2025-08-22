9cad4c51fa4d4021e3f051cb49dee77e808a1baa
gowthams369 <project3690000@gmail.com>
2025-08-22 08:32:02 +0530

    imported authrouter added dotenv middleware

--- src/app.js
+++ src/app.js
@@ -1,13 +1,19 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+import { authRouter } from "./routes/authRoute.js";
 
 const app = express();
+dotenv.config();
 const port = process.env.PORT || 5001;
 
 app.use(cors());
-app.use(cookieParser());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use("/api", authRouter);
 
 app.get("/", (req, res) => {
   console.log("App is running");
